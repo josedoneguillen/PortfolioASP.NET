a29d4d3d238b8b9a92ddb83cde7704dcd922d510
Jose Done <josedoneguillen1992@hotmail.com>
2023-05-04 10:09:31 -0400

    Setting up infrastructure layer with NugetPackages, Entity Framework and DbContext

---
+++ Portfolio.Domain/Repository/IBaseRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Portfolio.Domain.Repository
+{
+    public interface IBaseRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity> getEntityByID(int id);
+        Task<IEnumerable<TEntity>> getAll();
+        Task<TEntity> find(Expression<Func<TEntity, bool>> filter);
+        Task save(TEntity entity);
+        Task save(params TEntity[] entities);
+        Task update(TEntity entity);
+        Task update(params TEntity[] entities);
+        Task<bool> exist(Expression<Func<TEntity, bool>> filter);
+        Task saveChanges();
+    }
+}
